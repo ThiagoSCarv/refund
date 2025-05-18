ea0a848ba57f2658881a5e80500953a0622186f7
Thiago Soares <thiagocarv4@gmail.com>
2025-05-18 10:26:04 -0300

    criando not found

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,19 +2,29 @@ import { useState } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
-export function SignIn() {
+export function SignUp() {
+	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
 	function onSubmit(e: React.FormEvent) {
 		e.preventDefault();
 
-		console.log(email, password);
+		console.log(name, email, password, passwordConfirm);
 	}
 
 	return (
 		<form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+			<Input
+				required
+				legend="Nome"
+				type="text"
+				placeholder="Seu Nome"
+				onChange={(e) => setName(e.target.value)}
+			/>
+
 			<Input
 				required
 				legend="E-mail"
@@ -31,15 +41,23 @@ export function SignIn() {
 				onChange={(e) => setPassword(e.target.value)}
 			/>
 
+			<Input
+				required
+				legend="Confirmação da Senha"
+				type="password"
+				placeholder="123456"
+				onChange={(e) => setPasswordConfirm(e.target.value)}
+			/>
+
 			<Button type="submit" isLoading={isLoading}>
-				Entrar
+				Cadastrar
 			</Button>
 
 			<a
-				href="/signup"
+				href="/"
 				className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
 			>
-				Criar Conta
+				Já tenho uma conta
 			</a>
 		</form>
 	);
