842423085e0371381849973c324a43f53306f8cc
Thiago Soares <thiagocarv4@gmail.com>
2025-05-18 14:32:10 -0300

    criando component upload

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -6,6 +6,8 @@ import { Input } from "../components/Input";
 
 import { Select } from "../components/Select";
 
+import { Upload } from "../components/Upload";
+
 export function Refund() {
 	const [category, setCategory] = useState("");
 
@@ -23,18 +25,25 @@ export function Refund() {
 				</p>
 			</header>
 			<Input required legend="Nome da Solicitação" />
-			<Select
-				required
-				legend="Categoria"
-				value={category}
-				onChange={(e) => setCategory(e.target.value)}
-			>
-				{CATEGORIES_KEYS.map((category) => (
-					<option key={category} value={category}>
-						{CATEGORIES[category].name}
-					</option>
-				))}
-			</Select>
+
+			<div className="flex gap-4">
+				<Select
+					required
+					legend="Categoria"
+					value={category}
+					onChange={(e) => setCategory(e.target.value)}
+				>
+					{CATEGORIES_KEYS.map((category) => (
+						<option key={category} value={category}>
+							{CATEGORIES[category].name}
+						</option>
+					))}
+				</Select>
+
+				<Input legend="Valor" required />
+			</div>
+
+			<Upload />
 		</form>
 	);
 }
