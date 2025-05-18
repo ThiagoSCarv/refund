fbac7b08eda0a89fcfec8fa244cf124a8f7ea443
Thiago Soares <thiagocarv4@gmail.com>
2025-05-18 11:31:58 -0300

    criando AppLayout

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from "react-router";
 
 import { Refund } from "../pages/Refund";
 
+import { AppLayout } from "../components/AppLayout";
+
 import { NotFound } from "../pages/NotFound";
 
 export function EmployeeRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<Refund />} />
+			<Route path="/" element={<AppLayout />}>
+				<Route path="/" element={<Refund />} />
+			</Route>
 
+			
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
