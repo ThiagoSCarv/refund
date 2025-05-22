1255ab94a707ff7a1335529ce1a4e27d2ed5324b
Thiago Soares <thiagocarv4@gmail.com>
2025-05-22 14:49:17 -0300

    lidando com rotas de acordo com role do usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,13 +10,30 @@ import { Loading } from "../components/Loading";
 
 const isLoading = false;
 
+const session = {
+	user: {
+		role: "manager",
+	},
+};
+
 export function Routes() {
+	function Route() {
+		switch (session?.user.role) {
+			case "employee":
+				return <EmployeeRoutes />;
+			case "manager":
+				return <ManagerRoutes />;
+			default:
+				return <AuthRoutes />;
+		}
+	}
+
 	if (isLoading) {
 		return <Loading />;
 	}
 	return (
 		<BrowserRouter>
-			<AuthRoutes />
+			<Route />
 		</BrowserRouter>
 	);
 }
