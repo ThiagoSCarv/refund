c2522827b2ea1c5c5456c4b088980ed52f83a966
ThiagoSCarv <thiagocarv4@gmail.com>
2025-05-30 18:40:54 -0300

    cadastrando usuario

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,64 +1,111 @@
-import { useState } from "react";
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
+import { AxiosError } from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { ZodError, z } from 'zod'
+import { Button } from '../components/Button'
+import { Input } from '../components/Input'
+import { api } from '../services/api'
+
+const SignUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: 'Informe o nome' }),
+    email: z.string().email({ message: 'E-mail inválido' }),
+    password: z
+      .string()
+      .min(6, { message: 'Senha deve ter pelo menos 6 digítos' }),
+    passwordConfirm: z.string({ message: 'Confirme a senha' }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: 'As senhas não são iguais',
+    path: ['passwordConfirm'],
+  })
 
 export function SignUp() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [passwordConfirm, setPasswordConfirm] = useState("");
-	const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
-	function onSubmit(e: React.FormEvent) {
-		e.preventDefault();
+  const navigate = useNavigate()
 
-		console.log(name, email, password, passwordConfirm);
-	}
+  async function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
 
-	return (
-		<form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-			<Input
-				required
-				legend="Nome"
-				type="text"
-				placeholder="Seu Nome"
-				onChange={(e) => setName(e.target.value)}
-			/>
+    try {
+      setIsLoading(true)
 
-			<Input
-				required
-				legend="E-mail"
-				type="email"
-				placeholder="seu@email.com"
-				onChange={(e) => setEmail(e.target.value)}
-			/>
+      const data = SignUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      })
 
-			<Input
-				required
-				legend="Senha"
-				type="password"
-				placeholder="123456"
-				onChange={(e) => setPassword(e.target.value)}
-			/>
+      await api.post('/users', data)
 
-			<Input
-				required
-				legend="Confirmação da Senha"
-				type="password"
-				placeholder="123456"
-				onChange={(e) => setPasswordConfirm(e.target.value)}
-			/>
+      if (confirm('Cadastro realizado com sucesso. Ir para tela de login?')) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
 
-			<Button type="submit" isLoading={isLoading}>
-				Cadastrar
-			</Button>
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message)
+      }
 
-			<a
-				href="/"
-				className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
-			>
-				Já tenho uma conta
-			</a>
-		</form>
-	);
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="Nome"
+        type="text"
+        placeholder="Seu Nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Input
+        required
+        legend="Confirmação da Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Cadastrar
+      </Button>
+
+      <a
+        href="/"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Já tenho uma conta
+      </a>
+    </form>
+  )
 }
