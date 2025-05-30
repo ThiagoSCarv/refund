737dde9c21371729391019424744e5250e632753
ThiagoSCarv <thiagocarv4@gmail.com>
2025-05-30 20:28:28 -0300

    validação dos dados do usuario

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,39 +1,43 @@
 import { useActionState } from 'react'
+import { ZodError, z } from 'zod'
+import { email } from 'zod/v4'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
 
+const signInSchema = z.object({
+  email: z.string().email({ message: 'Este e-mail não é valido' }),
+  password: z.string().trim().min(1, { message: 'Informe a senha' }),
+})
+
 export function SignIn() {
-  const [state, formAction, isLoading] = useActionState(signIn, {
-    email: '',
-    password: '',
-  })
+  const [state, formAction, isLoading] = useActionState(signIn, null)
 
-  function signIn(prevState: any, formData: FormData) {
-    const email = formData.get('email')
-    const password = formData.get('password')
+  function signIn(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      })
+    } catch (error) {
+      console.log(error)
 
-    return { email, password }
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message }
+      }
+
+      return { message: 'Não foi possível entrar' }
+    }
   }
 
   return (
     <form action={formAction} className="w-full flex flex-col gap-4">
-      <Input
-        name="email"
-        required
-        legend="E-mail"
-        type="email"
-        placeholder="seu@email.com"
-				defaultValue={String(state.email)}
-      />
+      <Input name="email" required legend="E-mail" type="email" />
 
-      <Input
-        name="password"
-        required
-        legend="Senha"
-        type="password"
-        placeholder="123456"
-				defaultValue={String(state.password)}
-      />
+      <Input name="password" required legend="Senha" type="password" />
+
+			<p className='text-sm text-red-600 text-center my-4 font-medium'>
+				{state?.message}
+			</p>
 
       <Button type="submit" isLoading={isLoading}>
         Entrar
