b9627bdb74b1184c5259439d82ad1aa6f16ba279
ThiagoSCarv <thiagocarv4@gmail.com>
2025-05-30 20:34:18 -0300

    requisição de signIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,9 @@
+import { AxiosError } from 'axios'
 import { useActionState } from 'react'
 import { ZodError, z } from 'zod'
-import { email } from 'zod/v4'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
+import { api } from '../services/api'
 
 const signInSchema = z.object({
   email: z.string().email({ message: 'Este e-mail não é valido' }),
@@ -12,17 +13,24 @@ const signInSchema = z.object({
 export function SignIn() {
   const [state, formAction, isLoading] = useActionState(signIn, null)
 
-  function signIn(_: any, formData: FormData) {
+  async function signIn(_: any, formData: FormData) {
     try {
       const data = signInSchema.parse({
         email: formData.get('email'),
         password: formData.get('password'),
       })
+
+			const response = await api.post("/sessions", data)
+			console.log(response.data)
     } catch (error) {
       console.log(error)
 
       if (error instanceof ZodError) {
         return { message: error.issues[0].message }
+      }
+
+			if (error instanceof AxiosError) {
+        return { message: error.response?.data.message }
       }
 
       return { message: 'Não foi possível entrar' }
